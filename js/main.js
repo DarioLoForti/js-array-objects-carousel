6c03438ce747a7ca8fe008048782e7c4328e03db
DarioLoForti <lofortidario@gmail.com>
2023-12-05 15:56:17 +0100

    mileston 1

--- js/main.js
+++ js/main.js
@@ -21,3 +21,60 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+
+
+// Milestone 1: Aggiungiamo un contatore per tenere traccia dell'immagine attuale
+let currentIndex = 0;
+
+// Aggiungiamo l'ascoltatore degli eventi ai pulsanti next e prev
+document.querySelector('.next').addEventListener('click', showNext);
+document.querySelector('.prev').addEventListener('click', showPrev);
+
+// Funzione per mostrare l'immagine successiva
+function showNext() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+}
+
+// Funzione per mostrare l'immagine precedente
+function showPrev() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+}
+
+// Funzione per aggiornare il carosello
+function updateCarousel() {
+    const items = document.querySelectorAll('.item');
+    items.forEach((item, index) => {
+        item.classList.remove('active');
+        if (index === currentIndex) {
+            item.classList.add('active');
+        }
+    });
+}
+
+for(let key in images){
+    let img = images[key];
+   
+}
+for(i=0; i<images.length; i++){
+
+    let div = document.querySelectorAll('.item.active');
+    
+    let image = `<img src=./${images[i].image} alt="${images[i].title}">`;
+    div.innerHTML = image;
+    console.log(div);
+
+    let description = document.querySelectorAll('position-absolute');
+
+    let title = `<h2>${images[i].title}</h2>`;
+    let text = `<h5>${images[i].text}</h5>`;
+
+    description.innerHTML = title + text;
+
+    console.log(description);
+    
+
+
+}
